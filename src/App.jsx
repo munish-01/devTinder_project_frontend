cdced7290c34d99ce0fb95c5d8fd910cd02c8cc0
Munish Kumar <munish.dev@gmail.com>
2026-01-29 16:16:30 +0530

    tailwindcss + daisyui + created navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Navbar from "./Navbar";
+
 function App() {
 
   return (
     <>
-      <h1>Hello world!!!</h1>
+    <Navbar/>
+      <h1 className="text-3xl font-bold underline">Hello world!!!</h1>
     </>
   );
 }
