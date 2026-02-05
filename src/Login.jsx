a0d6b119bb99740c30d22353ecaeaec42984f6b8
Munish Kumar <munish.dev@gmail.com>
2026-02-05 12:09:24 +0530

    installed axios

---
+++ src/Login.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-6">
+        <legend className="fieldset-legend text-2xl">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          value={emailId}
+          type="email"
+          className="input"
+          placeholder="Email"
+          onChange={(e) => setEmailId(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          value={password}
+          type="password"
+          className="input"
+          placeholder="Password"
+          onChange={(e)=> setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4 w-full">Login</button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Login;
