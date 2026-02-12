f56b06635b71968c4ba703ec25137c0dfc38c256
Munish Kumar <munish.dev@gmail.com>
2026-02-12 16:37:28 +0530

    made BASE_URL Dynamic

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,11 @@
-export const BASE_URL = "/api"
+// production 
+
+// export const BASE_URL = "/api"
+
+// on local
+
+// export const BASE_URL = "http://localhost:7777"
+
+// dynamic for both
+
+export const BASE_URL = location.hostname === "localhost"  ? "http://localhost:7777" : "/api"
